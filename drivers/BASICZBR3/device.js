9b74a5a512a79c27373b5bc6764a9dea5000290b
Johan Bendz <Johan_Bendz@hotmail.com>
2020-12-02 00:25:11 +0100

    .

--- drivers/BASICZBR3/device.js
+++ drivers/BASICZBR3/device.js
@@ -11,11 +11,28 @@ class BasicSwitch extends ZigBeeDevice {
 		this.printNode();
 
 		// Register capabilities and listeners
-		if (this.hasCapability('onoff')) {
+		this.registerCapability('onoff', 'genOnOff', {
+			set: value => value ? 'on' : 'off',
+			setParser: () => ({}),
+			get: 'onOff',
+			reportParser: value => value === 1,
+		});
+
+/* 		if (this.hasCapability('onoff')) {
 			this.registerCapability('onoff', 'genOnOff');
-		};
+			this.registerAttrReportListener('genOnOff', 'onoff', 0, 60, null,
+			this.onOff_Report.bind(this))
+			.catch(err => {
+				this.error('failed to register report listener onOff', err);
+			});
+		}; */
 
 	}
+
+/* 	// Handle reports
+	onOff_Report(value) {
+		this.setCapabilityValue('onoff', value);
+	} */
 	
 }
 

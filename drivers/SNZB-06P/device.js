07ad5fcd7f071336c3d1bbbe8205a3e630ca1408
Johan Bendz <Johan_Bendz@hotmail.com>
2024-05-10 00:04:40 +0200

    v.0.0.33

--- drivers/SNZB-06P/device.js
+++ drivers/SNZB-06P/device.js
@@ -38,9 +38,16 @@ class SensorSNZB06P extends ZigBeeDevice {
     }
 
     onOccupancyAttributeReport(occupancy) {
-        this.log("Occupancy status:", occupancy);
-        this.setCapabilityValue('alarm_contact', occupancy)
+        this.log("Occupancy status:", occupancy.occupied);
+        this.setCapabilityValue('alarm_contact', occupancy.occupied)
         .catch(err => this.error('Error: could not set alarm_motion capability value', err));
+    
+        const alarmResetTime = this.getSetting('alarm_reset_time') || 3;
+        clearTimeout(this._motionAlarmTimeout);
+        this._motionAlarmTimeout = setTimeout(() => {
+            this.setCapabilityValue('alarm_contact', false)
+            .catch(err => this.error('Error: could not set alarm_motion capability value', err));
+        }, alarmResetTime * 1000); // alarmResetTime is in seconds, so we multiply by 1000 to get milliseconds
     }
 
     onIASZoneStatusChangeNotification({zoneStatus, extendedStatus, zoneId, delay,}) {
